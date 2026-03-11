10d44c38de6aa70c4cac54b7cd98c7c742b3f2ff
julianarps <192204304+julianarps@users.noreply.github.com>
2026-03-11 12:42:04 +0100

    Add `logout`, `refreshToken`, and `profile` API methods to `api.js`.

--- JavaScript/api.js
+++ JavaScript/api.js
@@ -18,6 +18,48 @@ export async function login(username, password) {
     return response.json();
 }
 
+export async function logout(token) {
+    const response = await fetch(`${BASE_URL}/auth/logout`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${token}`,
+        },
+    })
+    
+    return response.json();
+}
+
+export async function refreshToken(refreshToken) {
+    const response = await fetch(`${BASE_URL}/auth/refresh`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${refreshToken}`,
+        },
+    })
+    
+    return response.json();
+}
+
+export async function profile(token) {
+    const response = await fetch(`${BASE_URL}/auth/profile`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${token}`,
+        },
+    });
+    
+    return response.json();
+}
+
 export default {
-    login
+    login,
+    logout,
+    refreshToken,
+    profile,
 }
